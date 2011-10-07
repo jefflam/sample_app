12d0dc9e1b6930be818868c138db61c0c013624c
Jeff Lam <jefflamth@gmail.com>
2011-10-07 11:15:42 +0200

    updated sign up

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -6,25 +6,25 @@ describe "Users" do
       it "should not make a new user" do
         lambda do
           visit signup_path
-          fill_in "Name",             :with => ""
-          fill_in "Email",            :with => ""
-          fill_in "Password",         :with => ""
-          fill_in "Confirmation",     :with => ""
+          fill_in "Name",                       :with => ""
+          fill_in "Email",                      :with => ""
+          fill_in "Password",                   :with => ""
+          fill_in "Retype your password again", :with => ""
           click_button              
           response.should render_template('users/new')
           response.should have_selector('div#error_explanation')
         end.should_not change(User, :count)
       end
     end
-    
+  
     describe "success" do
       it "should make a new user" do
        lambda do
           visit signup_path
-          fill_in "Name",             :with => "Example User"
-          fill_in "Email",            :with => "user@example.com"
-          fill_in "Password",         :with => "foobar"
-          fill_in "Confirmation",     :with => "foobar"
+          fill_in "Name",                       :with => "Example User"
+          fill_in "Email",                      :with => "user@example.com"
+          fill_in "Password",                   :with => "foobar"
+          fill_in "Retype your password again", :with => "foobar"
           click_button              
           response.should have_selector('div.flash.success',
                                         :content => "Welcome" )   
@@ -32,6 +32,6 @@ describe "Users" do
         end.should change(User, :count).by(1)
       end
     end
-    
+  
   end
 end
